6ceeb4a29e1daadf3a49d1a5383b0159c7d5d57c
=tdu92 <tdu92>
2024-04-12 21:32:06 +0700

    first commit

---
+++ src/main/java/com/example/demospringmvc/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.demospringmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
